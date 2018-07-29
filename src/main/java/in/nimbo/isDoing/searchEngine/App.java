6deed7a24401764533422426afa56d33275e49c7
MSDehghan <msdn2013@gmail.com>
2018-07-29 16:15:59 +0430

    init commit

---
+++ src/main/java/in/nimbo/isDoing/searchEngine/App.java
@@ -0,0 +1,13 @@
+package in.nimbo.isDoing.searchEngine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
