ab09c0b115c33849e0e5f34ee92164eb77bcd561
MSDehghan <msdn2013@gmail.com>
2018-08-07 17:14:32 +0430

    Create First Test

---
+++ src/test/java/in/nimbo/isDoing/searchEngine/crawler/lru/CaffeineLRUTest.java
@@ -0,0 +1,14 @@
+package in.nimbo.isDoing.searchEngine.crawler.lru;
+
+import org.junit.Test;
+
+public class CaffeineLRUTest {
+
+    @Test
+    public void isRecentlyUsed() {
+    }
+
+    @Test
+    public void setUsed() {
+    }
+}
