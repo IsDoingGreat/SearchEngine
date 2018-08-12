5b748db1c91d0bed6e75e0dd01429c1bace6e7a2
Reza Bayat <reza.bayat76@gmail.com>
2018-08-12 16:56:17 +0430

    [DEVELOP <CREATE FILE>] -> test of CaffeineDuplicateChecker

---
+++ src/test/java/in/nimbo/isDoing/searchEngine/crawler/duplicate_checker/CaffeineDuplicateCheckerTest.java
@@ -0,0 +1,107 @@
+package in.nimbo.isDoing.searchEngine.crawler.duplicate_checker;
+
+import in.nimbo.isDoing.searchEngine.engine.Engine;
+import in.nimbo.isDoing.searchEngine.engine.interfaces.Configs;
+import in.nimbo.isDoing.searchEngine.pipeline.Console.ConsoleOutput;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.URL;
+
+public class CaffeineDuplicateCheckerTest {
+
+    @Test
+    public void checkDuplicateAndSet() throws Exception  {
+        Engine.start(new ConsoleOutput(), new Configs() {
+            @Override
+            public String get(String key) {
+                switch (key) {
+                    case "crawler.persister.db.hbase.crawledLink.tableName":
+                        return "crawledLink";
+
+                    case "crawler.persister.db.hbase.crawledLink.columnFamily":
+                        return "partition";
+
+                    case "crawler.persister.db.hbase.crawledLink.qualifier":
+                        return "number";
+
+
+                    case "crawler.persister.db.hbase.pages.tableName":
+                        return "pages";
+
+                    case "crawler.persister.db.hbase.pages.columnFamily":
+                        return "data";
+
+                    case "crawler.persister.db.hbase.pages.qualifiers":
+                        return "link;context";
+
+
+                    case "crawler.persister.db.hbase.backLinks.tableName":
+                        return "backLinks";
+
+                    case "crawler.persister.db.hbase.backLinks.columnFamily":
+                        return "links";
+
+
+                    case "hbase.site":
+                        return "hbase-site.xml";
+
+                    case "crawler.persister.db.hbase.flushNumberLimit":
+                        return "150";
+                }
+                throw new RuntimeException();
+            }
+
+            @Override
+            public String get(String key, String value) {
+                switch (key) {
+                    case "crawler.persister.db.hbase.crawledLink.tableName":
+                        return "crawledLink";
+
+                    case "crawler.persister.db.hbase.crawledLink.columnFamily":
+                        return "partition";
+
+                    case "crawler.persister.db.hbase.crawledLink.qualifier":
+                        return "number";
+
+
+                    case "crawler.persister.db.hbase.pages.tableName":
+                        return "pages";
+
+                    case "crawler.persister.db.hbase.pages.columnFamily":
+                        return "data";
+
+                    case "crawler.persister.db.hbase.pages.qualifiers":
+                        return "link;context";
+
+
+                    case "crawler.persister.db.hbase.backLinks.tableName":
+                        return "backLinks";
+
+                    case "crawler.persister.db.hbase.backLinks.columnFamily":
+                        return "links";
+
+
+                    case "hbase.site":
+                        return "hbase-site.xml";
+
+                    case "crawler.persister.db.hbase.flushNumberLimit":
+                        return "150";
+                }
+                throw new RuntimeException();
+            }
+        });
+
+        URL url = new URL("https://www.tutorialspoint.com/hbase/hbase_delete_data.htm");
+
+        DuplicateChecker duplicateChecker = new CaffeineDuplicateChecker();
+        Assert.assertFalse(duplicateChecker.checkDuplicateAndSet(url));
+        Assert.assertTrue(duplicateChecker.checkDuplicateAndSet(url));
+
+    }
+
+    @Test
+    public void stop() {
+
+    }
+}
