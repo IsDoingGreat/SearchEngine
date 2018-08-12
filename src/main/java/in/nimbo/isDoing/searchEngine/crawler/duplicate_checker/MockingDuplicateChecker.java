1a803135ca46d37bb3b3e96f7958b392373c6afc
Reza Bayat <reza.bayat76@gmail.com>
2018-08-12 16:57:09 +0430

    [DEVELOP <UPDATE FILE>] -> refactor isDuplicate method to checkDuplicateAndSet

--- src/main/java/in/nimbo/isDoing/searchEngine/crawler/duplicate_checker/MockingDuplicateChecker.java
+++ src/main/java/in/nimbo/isDoing/searchEngine/crawler/duplicate_checker/MockingDuplicateChecker.java
@@ -3,11 +3,14 @@ package in.nimbo.isDoing.searchEngine.crawler.duplicate_checker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URL;
+
 public class MockingDuplicateChecker implements DuplicateChecker {
     private final static Logger logger = LoggerFactory.getLogger(MockingDuplicateChecker.class);
 
+
     @Override
-    public boolean isDuplicate(String page) throws Exception {
+    public boolean checkDuplicateAndSet(URL url) throws Exception {
         return false;
     }
 
