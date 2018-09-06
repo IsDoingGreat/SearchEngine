21b6471fd7260da32f4e34e8a5a438d664eff3f8
fahmide <fahmide@ce.sharif.edu>
2018-09-06 12:36:09 +0430

    Create JmxCounter class for monitoring new reader.

--- newsReader/src/main/java/in/nimbo/isDoing/searchEngine/newsReader/controller/JmxCounter.java
+++ newsReader/src/main/java/in/nimbo/isDoing/searchEngine/newsReader/controller/JmxCounter.java
@@ -8,27 +8,34 @@ import static com.codahale.metrics.MetricRegistry.name;
 public class JmxCounter {
     public static final MetricRegistry metricRegistry = new MetricRegistry();
     private static final Counter successfulItemsOfHBasePersister =
-            metricRegistry.counter(name(Counter.class, "persisted items in HBase"));
+            metricRegistry.counter(name(Counter.class, "Persisted items in HBase"));
     private static final Counter successfulItemsOfElasticPersister =
-            metricRegistry.counter(name(Counter.class, "persisted items in Elastichsearch"));
-    private static final Counter invalidTexts =
-            metricRegistry.counter(name(Counter.class, "Null pages"));
+            metricRegistry.counter(name(Counter.class, "Persisted items in Elasticsearch"));
+    private static final Counter invalidChannels =
+            metricRegistry.counter(name(Counter.class, "Null channel"));
+    private static final Counter invalidItems =
+            metricRegistry.counter(name(Counter.class, "Null date"));
+    private static final Counter duplicateItems =
+            metricRegistry.counter(name(Counter.class, "Duplicate items"));
 
-    public static void increamentSuccessfulItemsOfHBasePersister(int number) {
+    public static void incrementSuccessfulItemsOfHBasePersister(int number) {
         successfulItemsOfHBasePersister.inc(number);
     }
 
-    public static void increamentSuccessfulItemsOfHBasePersister() {
-        successfulItemsOfHBasePersister.inc();
-    }
 
-
-    public static void setSuccessfulItemsOfElasticPersister(int number) {
+    public static void incrementSuccessfulItemsOfElasticPersister(int number) {
         successfulItemsOfElasticPersister.inc(number);
     }
 
-    public static void getInvalidTexts() {
-        successfulItemsOfElasticPersister.inc();
+    public static void incrementInvalidChannels() {
+        invalidChannels.inc();
     }
 
+    public static void incrementInvalidItems() {
+        invalidItems.inc();
+    }
+
+    public static void incrementDuplicateItems() {
+        duplicateItems.inc();
+    }
 }
