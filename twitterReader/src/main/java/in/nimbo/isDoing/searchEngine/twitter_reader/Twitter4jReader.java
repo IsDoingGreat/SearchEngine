c2b3e9d613e0143b5ac457b9dbe1fd6335d2f692
fahmide <fahmide@ce.sharif.edu>
2018-09-10 19:52:42 +0430

    update twitter reader

--- twitterReader/src/main/java/in/nimbo/isDoing/searchEngine/twitter_reader/Twitter4jReader.java
+++ twitterReader/src/main/java/in/nimbo/isDoing/searchEngine/twitter_reader/Twitter4jReader.java
@@ -3,6 +3,7 @@ package in.nimbo.isDoing.searchEngine.twitter_reader;
 import in.nimbo.isDoing.searchEngine.elastic.ElasticClient;
 import in.nimbo.isDoing.searchEngine.engine.Engine;
 import in.nimbo.isDoing.searchEngine.kafka.KafkaProducerController;
+import in.nimbo.isDoing.searchEngine.pipeline.Console.ConsoleOutput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import twitter4j.*;
@@ -14,24 +15,25 @@ public class Twitter4jReader {
     private static final Logger logger = LoggerFactory.getLogger(Twitter4jReader.class);
     private static ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
     private static ElasticTwitterPersister elasticTwitterPersister = new ElasticTwitterPersister();
+    private TwitterStream twitterStream;
     private static KafkaProducerController kafkaProducer = new KafkaProducerController(
             Engine.getConfigs().get("kafka.brokers"),
             Engine.getConfigs().get("twitterReader.kafka.producerClientId"),
             Engine.getConfigs().get("twitterReader.kafka.topicName"));
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Engine.start(new ConsoleOutput());
+
         configurationBuilder.setDebugEnabled(true)
                 .setOAuthConsumerKey(Engine.getConfigs().get("twitter.api.key"))
                 .setOAuthConsumerSecret(Engine.getConfigs().get("twitter.api.secret.key"))
                 .setOAuthAccessToken(Engine.getConfigs().get("twitter.api.access.token"))
                 .setOAuthAccessTokenSecret(Engine.getConfigs().get("twitter.api.access.token.secret"));
 
-        Twitter4jReader.getTwitterStream();
     }
 
-
-    private static void getTwitterStream() {
+    void getTwitterStream() {
         StatusListener listener = new StatusListener() {
             @Override
             public void onStatus(Status status) {
@@ -75,8 +77,16 @@ public class Twitter4jReader {
         };
 
         TwitterStreamFactory twitterStreamFactory = new TwitterStreamFactory(configurationBuilder.build());
-        TwitterStream twitterStream = twitterStreamFactory.getInstance();
+        twitterStream = twitterStreamFactory.getInstance();
         twitterStream.addListener(listener);
         twitterStream.filter(new FilterQuery().language("en"));
+
+    }
+
+    void stopGetTwitterStream() {
+        if (kafkaProducer != null)
+            kafkaProducer.stop();
+        if (twitterStream != null)
+            twitterStream.shutdown();
     }
 }
