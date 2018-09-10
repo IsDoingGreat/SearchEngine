6d303a720bf647be05f39ddd29d39f925e55d0b2
fahmide <fahmide@ce.sharif.edu>
2018-09-10 19:24:53 +0430

    new twitter reader with the twitter api added

--- twitterReader/src/main/java/in/nimbo/isDoing/searchEngine/twitter_reader/Twitter4jReader.java
+++ twitterReader/src/main/java/in/nimbo/isDoing/searchEngine/twitter_reader/Twitter4jReader.java
@@ -1,38 +1,51 @@
 package in.nimbo.isDoing.searchEngine.twitter_reader;
 
+import in.nimbo.isDoing.searchEngine.elastic.ElasticClient;
+import in.nimbo.isDoing.searchEngine.engine.Engine;
+import in.nimbo.isDoing.searchEngine.kafka.KafkaProducerController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
+import java.util.concurrent.ExecutionException;
+
 public class Twitter4jReader {
     private static final Logger logger = LoggerFactory.getLogger(Twitter4jReader.class);
     private static ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+    private static ElasticTwitterPersister elasticTwitterPersister = new ElasticTwitterPersister();
+    private static KafkaProducerController kafkaProducer = new KafkaProducerController(
+            Engine.getConfigs().get("kafka.brokers"),
+            Engine.getConfigs().get("twitterReader.kafka.producerClientId"),
+            Engine.getConfigs().get("twitterReader.kafka.topicName"));
 
-    private Twitter4jReader() {}
 
-    public static void main(String[] args) throws Exception {
-        if (args.length < 4 ){
-            System.out.println("Invalid Input");
-            return;
-        }
-
+    public static void main(String[] args) {
         configurationBuilder.setDebugEnabled(true)
-                .setOAuthConsumerKey(args[0])
-                .setOAuthConsumerSecret(args[1])
-                .setOAuthAccessToken(args[2])
-                .setOAuthAccessTokenSecret(args[3]);
+                .setOAuthConsumerKey(Engine.getConfigs().get("twitter.api.key"))
+                .setOAuthConsumerSecret(Engine.getConfigs().get("twitter.api.secret.key"))
+                .setOAuthAccessToken(Engine.getConfigs().get("twitter.api.access.token"))
+                .setOAuthAccessTokenSecret(Engine.getConfigs().get("twitter.api.access.token.secret"));
+
         Twitter4jReader.getTwitterStream();
     }
 
 
-    public static void getTwitterStream() {
+    private static void getTwitterStream() {
         StatusListener listener = new StatusListener() {
             @Override
             public void onStatus(Status status) {
-                if (status.getLang().equals("en")) {
-                    System.out.println(status.getText());
+                try {
+                    elasticTwitterPersister.persist(status);
+                    kafkaProducer.produce(status.getText());
+                } catch (ExecutionException e) {
+                    logger.error("kafka producer execution exception.", e);
+                } catch (InterruptedException e) {
+                    logger.error("kafka producer interrupted exception.", e);
+                } catch (ElasticTwitterPersister.ElasticTwitterPersisterException e) {
+                    logger.error("elasticSearch twitter persister was intrupted", e);
                 }
+
             }
 
             @Override
@@ -57,13 +70,13 @@ public class Twitter4jReader {
 
             @Override
             public void onException(Exception e) {
-                e.printStackTrace();
+                logger.error("twitter stream exception.", e);
             }
         };
 
         TwitterStreamFactory twitterStreamFactory = new TwitterStreamFactory(configurationBuilder.build());
         TwitterStream twitterStream = twitterStreamFactory.getInstance();
         twitterStream.addListener(listener);
-        twitterStream.sample();
+        twitterStream.filter(new FilterQuery().language("en"));
     }
 }
