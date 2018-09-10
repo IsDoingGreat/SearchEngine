da701034db3953195fa12bb8d8b1dfb18958fbbb
fahmide <fahmide@ce.sharif.edu>
2018-09-10 20:20:40 +0430

    twitter reader bug fixed

--- twitterReader/src/main/java/in/nimbo/isDoing/searchEngine/twitter_reader/Twitter4jReader.java
+++ twitterReader/src/main/java/in/nimbo/isDoing/searchEngine/twitter_reader/Twitter4jReader.java
@@ -2,6 +2,7 @@ package in.nimbo.isDoing.searchEngine.twitter_reader;
 
 import in.nimbo.isDoing.searchEngine.elastic.ElasticClient;
 import in.nimbo.isDoing.searchEngine.engine.Engine;
+import in.nimbo.isDoing.searchEngine.engine.SystemConfigs;
 import in.nimbo.isDoing.searchEngine.kafka.KafkaProducerController;
 import in.nimbo.isDoing.searchEngine.pipeline.Console.ConsoleOutput;
 import org.slf4j.Logger;
@@ -13,24 +14,26 @@ import java.util.concurrent.ExecutionException;
 
 public class Twitter4jReader {
     private static final Logger logger = LoggerFactory.getLogger(Twitter4jReader.class);
-    private static ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
-    private static ElasticTwitterPersister elasticTwitterPersister = new ElasticTwitterPersister();
+    private ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+    private ElasticTwitterPersister elasticTwitterPersister = new ElasticTwitterPersister();
     private TwitterStream twitterStream;
-    private static KafkaProducerController kafkaProducer = new KafkaProducerController(
+    private KafkaProducerController kafkaProducer = new KafkaProducerController(
             Engine.getConfigs().get("kafka.brokers"),
             Engine.getConfigs().get("twitterReader.kafka.producerClientId"),
             Engine.getConfigs().get("twitterReader.kafka.topicName"));
 
-
-    public static void main(String[] args) throws Exception {
-        Engine.start(new ConsoleOutput());
-
+    Twitter4jReader() {
         configurationBuilder.setDebugEnabled(true)
                 .setOAuthConsumerKey(Engine.getConfigs().get("twitter.api.key"))
                 .setOAuthConsumerSecret(Engine.getConfigs().get("twitter.api.secret.key"))
                 .setOAuthAccessToken(Engine.getConfigs().get("twitter.api.access.token"))
                 .setOAuthAccessTokenSecret(Engine.getConfigs().get("twitter.api.access.token.secret"));
+    }
 
+    public static void main(String[] args) throws Exception {
+        Engine.start(new ConsoleOutput(),new SystemConfigs("twitterreader"));
+
+        Engine.getInstance().startService(new Twitter4jReaderService());
     }
 
     void getTwitterStream() {
