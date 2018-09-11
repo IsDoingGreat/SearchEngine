25d57c8cafafc2436268b872b1de975422cd55e2
Reza Bayat <reza.bayat76@gmail.com>
2018-09-11 12:36:50 +0430

    [DEVELOP <UPDATE FILES>] -> debug to put in habse and kafka

--- twitterReader/src/main/java/in/nimbo/isDoing/searchEngine/twitter_reader/TwitterStreamReader.java
+++ twitterReader/src/main/java/in/nimbo/isDoing/searchEngine/twitter_reader/TwitterStreamReader.java
@@ -1,19 +1,24 @@
 package in.nimbo.isDoing.searchEngine.twitter_reader;
 
-import org.slf4j.Logger;
+import in.nimbo.isDoing.searchEngine.engine.Engine;
+import in.nimbo.isDoing.searchEngine.kafka.KafkaProducerController;
 import org.slf4j.LoggerFactory;
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
+import java.util.concurrent.ExecutionException;
+
 public class TwitterStreamReader {
-
-//    private static final Logger logger = LoggerFactory.getLogger(TwitterStreamReader.class);
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(TwitterStreamReader.class);
     private static ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
-    private TwitterStreamReader() {
+    private static ElasticTwitterPersister elasticTwitterPersister = new ElasticTwitterPersister();
+    private static KafkaProducerController kafkaProducer = new KafkaProducerController(
+            Engine.getConfigs().get("kafka.brokers"),
+            Engine.getConfigs().get("twitterReader.kafka.producerClientId"),
+            Engine.getConfigs().get("twitterReader.kafka.topicName"));
 
-    }
-    public static void main(String[] args) throws Exception {
-        if (args.length < 4 ){
+    public static void main(String[] args) {
+        if (args.length < 4) {
             System.out.println("Invalid Input");
             return;
         }
@@ -27,40 +32,56 @@ public class TwitterStreamReader {
         TwitterStreamReader.getTwitterStream();
         System.out.println("started");
     }
+
     public static void getTwitterStream() {
         StatusListener listener = new StatusListener() {
             @Override
             public void onStatus(Status status) {
-                System.out.println("notEnglish");
-                if (status.getLang().equals("en")) {
+                System.out.println(status.getText());
+
+                try {
                     System.out.println(status.getText());
+                    elasticTwitterPersister.persist(status);
+                    kafkaProducer.produce(status.getText());
+                } catch (ExecutionException e) {
+                    logger.error("kafka producer execution exception.", e);
+                } catch (InterruptedException e) {
+                    logger.error("kafka producer interrupted exception.", e);
+                } catch (ElasticTwitterPersister.ElasticTwitterPersisterException e) {
+                    logger.error("elasticSearch twitter persister was intrupted", e);
                 }
             }
+
             @Override
             public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
                 System.out.println("onDeletionNotice");
             }
+
             @Override
             public void onTrackLimitationNotice(int i) {
                 System.out.println("onScrubGeo");
             }
+
             @Override
             public void onScrubGeo(long l, long l1) {
                 System.out.println("onScrubGeo");
             }
+
             @Override
             public void onStallWarning(StallWarning stallWarning) {
                 System.out.println("onStallWarning");
             }
+
             @Override
             public void onException(Exception e) {
                 System.out.println("Exception");
                 e.printStackTrace();
             }
         };
+
         TwitterStreamFactory twitterStreamFactory = new TwitterStreamFactory(configurationBuilder.build());
         TwitterStream twitterStream = twitterStreamFactory.getInstance();
         twitterStream.addListener(listener);
-        twitterStream.sample();
+        twitterStream.filter(new FilterQuery().language("en"));
     }
 }
