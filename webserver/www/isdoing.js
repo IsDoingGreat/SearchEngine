5f55f77682202f2cfb25705c7608cba8f0a5f179
MSDehghan <msdn2013@gmail.com>
2018-09-11 23:02:24 +0430

    update webserver

---
+++ webserver/www/isdoing.js
@@ -0,0 +1,8 @@
+function getError(msg) {
+    return "<div class=\"alert  alert-danger alert-dismissible fade show\" role=\"alert\">" +
+        "     <span class=\"badge badge-pill badge-danger\"> Error </span> " + msg +
+        "     <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">" +
+        "         <span aria-hidden=\"true\">×</span>" +
+        "     </button>" +
+        "</div>";
+}
